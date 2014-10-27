f2a6f7969f8f934842ea211513cd2366a748a3d3
Acconut <maerious@gmail.com>
2014-10-27 23:18:35 +0100

    Add travis-release.sh

---
+++ travis-release.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+# Test if we have an release commit
+msg=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
+if [[ $msg == Release* ]] || [[ $msg == release* ]]
+then
+    echo "Found release commit"
+else
+    echo "No release commit found"
+    exit 0
+fi
+
+# Get version from commit message
+version=$(echo $msg | awk '{print $2}')
+echo "Releasing ${version} ..."
+
+data="{\
+\"tag_name\":\"${version}\",\
+\"target_commitish\":\"${TRAVIS_COMMIT}\",\
+\"name\":\"${version}\",\
+\"body\":\"Release ${version}\"\
+}"
+
+body=$(curl \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -u "${OAUTH_TOKEN}:x-oauth-basic" \
+    -d "$data" \
+    https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases)
+
+# Get release id
+release_id=$(echo $body | grep -oP "\"id\": \d+" | head -1 | awk '{print $2}')
+echo "Created release ${release_id}"
+
+# Get only repo name
+repo=$(echo $TRAVIS_REPO_SLUG | cut -d"/" -f2)
+
+function upload {
+    local name=$1
+    local content_type=$2
+
+    curl -iL "http://gobuild3.qiniudn.com/github.com/${TRAVIS_REPO_SLUG}/tag-v-${version}/${repo}-${name}" | \
+curl \
+    -X POST \
+    -u "${OAUTH_TOKEN}:x-oauth-basic" \
+    -H "Content-Type: ${content_type}" \
+    "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${release_id}/assets?name=${name}" \
+    -d @-
+    
+}
+
+# Upload builds from gobuild.io
+upload "windows-386.zip" "application/zip"
+upload "windows-amd64.zip" "application/zip"
+upload "linux-386.tar.gz", "application/gzip"
+upload "linux-amd64.tar.gz", "application/gzip"
+upload "linux-arm.tar.gz", "application/gzip"
+upload "darwin-386.zip", "application/zip"
+upload "darwin-amd64.zip", "application/zip"
